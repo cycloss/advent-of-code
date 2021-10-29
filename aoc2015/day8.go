52c122dd096a16564302c1dbe289435fe7697379
Ccyclos <50776059+lucas979797@users.noreply.github.com>
2021-10-29 10:00:31 +0100

    Adds day 8 solution

--- aoc2015/day8.go
+++ aoc2015/day8.go
@@ -6,8 +6,7 @@ import (
 )
 
 type token struct {
-	codeCount, charCount int
-	literal              string
+	codeCount, charCount, encodedCount int
 }
 
 type tokeniser struct {
@@ -15,8 +14,29 @@ type tokeniser struct {
 	bytes []byte
 }
 
+func main() {
+	var bytes, _ = ioutil.ReadFile("inputs/day8.txt")
+	var tokeniser = newTokeniser(bytes)
+	var tokens = tokeniser.tokeniseBytes()
+	var codeTotal, charTotal, encodedTotal = calculateTotal(tokens)
+	var res = codeTotal - charTotal
+	fmt.Printf("Result: %d\n", res)
+	// plus 2 for surrounding quotes
+	var encodedDiff = encodedTotal - codeTotal
+	fmt.Printf("Encoded diff: %d\n", encodedDiff)
+}
+
 func newTokeniser(b []byte) *tokeniser {
 	return &tokeniser{-1, b}
+}
+
+func (t *tokeniser) tokeniseBytes() []*token {
+	var tokens = []*token{}
+	for t.next() {
+		var token = t.getNextToken()
+		tokens = append(tokens, token)
+	}
+	return tokens
 }
 
 func (t *tokeniser) next() bool {
@@ -24,18 +44,55 @@ func (t *tokeniser) next() bool {
 	return t.index < len(t.bytes)
 }
 
-func main() {
-	var bytes, _ = ioutil.ReadFile("inputs/day8.txt")
-	var tokens = tokenise(bytes)
-	var totalCode, totalChar = calculateTotal(tokens)
-	var res = totalCode - totalChar
-	fmt.Println("Result: %d", res)
+func (t *tokeniser) current() byte {
+	return t.bytes[t.index]
 }
 
-func tokenise(bytes []byte) []token {
+const backslash = byte('\\')
+const quote = byte('"')
+const newline byte = '\n'
 
+func (t *tokeniser) getNextToken() *token {
+
+	var current = t.current()
+	switch current {
+	case newline:
+		return &token{}
+	case backslash:
+		return t.handleEscaped()
+	case quote:
+		// add 3 as should be surrounding quote
+		return &token{codeCount: 1, encodedCount: 3}
+	default:
+		return &token{charCount: 1, codeCount: 1, encodedCount: 1}
+	}
 }
 
-func calculateTotal(tokens []token) (int, int) {
+func (t *tokeniser) handleEscaped() *token {
+	if !t.next() {
+		return &token{charCount: 1, encodedCount: 1, codeCount: 1}
+	}
+	var current = t.current()
+	switch current {
+	case backslash:
+		// must escape two back slashes
+		return &token{codeCount: 2, charCount: 1, encodedCount: 4}
+	case quote:
+		return &token{codeCount: 2, charCount: 1, encodedCount: 4}
+	default:
+		// must be hex sequence
+		t.index += 2
+		return &token{codeCount: 4, charCount: 1, encodedCount: 5}
+	}
+}
 
+func calculateTotal(tokens []*token) (int, int, int) {
+	var codeTotal, charTotal, encodedTotal int
+	for _, token := range tokens {
+		codeTotal += token.codeCount
+		charTotal += token.charCount
+		encodedTotal += token.encodedCount
+	}
+
+	return codeTotal, charTotal, encodedTotal
 }
